c6865eaf992c78a9aca6e67a4a1b661aff00d763
nickode <nicolasnsamaha@hotmail.com>
2020-10-03 17:59:38 -0400

    Fixed merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {CreateComponent} from '../app/create/create.component';
-<<<<<<< HEAD
+
 import { HomeComponent } from './home/home.component';
-=======
-<<<<<<< HEAD
+
+
 import { LobbyComponent } from './lobby/lobby.component';
-=======
->>>>>>> 0f77fa30f087aebdbbcd1f42e21be53dedf564ae
+
+
 import { JoinComponent } from './join/join.component';
->>>>>>> b6daf27d04f4b3216589f0f8f7a4cdf52a92101c
+
 
 
 
