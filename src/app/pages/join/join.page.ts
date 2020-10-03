f9c1bfadbd6cca1ce5b7be31ce2bdfeb34e9faf5
Cristofer Elhage <cristoferelhage@gmail.com>
2020-10-03 19:32:46 -0400

    copied code to transfer components to pages

---
+++ src/app/pages/join/join.page.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import {Player} from '../player';
+
+@Component({
+  selector: 'app-join',
+  templateUrl: './join.page.html',
+  styleUrls: ['./join.page.scss'],
+})
+export class JoinPage implements OnInit {
+
+  public game:any
+  public players = [1,2,3]
+  public hostname:string
+  public username:string
+
+  constructor(private http:HttpClient, private router:Router, private storage: Storage) { }
+
+  ngOnInit() {
+
+    this.storage.ready().then(() => {
+
+      this.storage.get('hostname').then(hostname => {
+        this.hostname = hostname
+
+        this.storage.get('username').then(username =>{
+          this.username = username
+
+        }).then(() => {
+          console.log(this.username)
+          console.log(this.hostname)
+
+          this.http.get(`http://localhost:3000/games/${this.hostname}`).subscribe(game => {
+            this.game = game
+          })
+
+        }).then(() => {
+          this.http.get(`http://localhost:3000/players/${this.hostname}`).subscribe(players => {
+            this.players
+          })
+        })
+      })
+    })
+  }
+
+  getPlayers()
+  {
+    return this.players
+  }
+
+  showPlayers()
+  {
+    console.log(this.players)
+  }
+
+  showGame()
+  {
+    console.log(this.game)
+  }
+}
